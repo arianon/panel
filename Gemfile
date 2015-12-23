4c93926c4020b4581fa1bc16bf02e4fac7147226
arianon <arianon@openmailbox.org>
2015-12-23 17:48:57 -0430

    config.rb: Parse the YAML as a RecursiveOpenStruct

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'recursive-open-struct'
--- lib/config.rb
+++ lib/config.rb
@@ -1,15 +1,4 @@
 require 'yaml'
+require 'recursive-open-struct'
 
-CONFIG = YAML.load_file(File.expand_path '../../config.yml', __FILE__).freeze
-
-module Color
-  CONFIG[:colors].each_pair do |name, color|
-    define_singleton_method(name) { format('#FF%06X', color) }
-  end
-end
-
-module Icon
-  CONFIG[:icons].each_pair do |name, icon|
-    define_singleton_method(name) { " #{icon} " }
-  end
-end
+CONFIG = RecursiveOpenStruct.new(YAML.load_file(File.expand_path('../../config.yml', __FILE__)))
