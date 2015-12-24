94be16f6ca7f0bf4f9b7b4a3c2c1b02d4b5c0e85
arianon <arianon@openmailbox.org>
2015-12-23 21:55:36 -0430

    Added: clock.rb widget

---
+++ lib/widgets/clock.rb
@@ -0,0 +1,20 @@
+require_relative '../config'
+
+class Clock
+  C = CONFIG.clock
+
+  def initialize
+    @icon = "%{F#{C.color}}%{R} #{C.icon} %{R}%{F-} "
+  end
+
+  def monitor
+    loop do
+      yield
+      sleep C.reload
+    end
+  end
+
+  def to_s
+    @icon + Time.now.strftime(C.format)
+  end
+end
