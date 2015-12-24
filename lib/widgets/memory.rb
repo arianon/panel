aee27a99f189cfa2ea29e276f840c172eafc7783
arianon <arianon@openmailbox.org>
2015-12-23 20:50:30 -0430

    memory.rb: Rewrite to obey config.yml

--- lib/widgets/memory.rb
+++ lib/widgets/memory.rb
@@ -1,35 +1,38 @@
-#!/usr/bin/ruby -wU
+require_relative '../config'
 
-module Memory
+class Memory
   REGEX = /^(?:MemTotal|MemFree|Buffers|Cached):\s+(\d+) kB/
+  C = CONFIG.memory
 
-  def self.readmemory
-    File.read('/proc/meminfo')
-      .scan(REGEX)
-      .flatten
-      .map!(&:to_f)
-      .map! { |n| n / 1024 }
+  def initialize
+    @total, @free, @buffers, @cached = File.read('/proc/meminfo')
+                                       .scan(REGEX)
+                                       .flatten
+                                       .map!(&:to_f)
+                                       .map! { |n| n / 1024 }
   end
 
-  def self.free
-    readmemory[1..-1].inject(:+)
+  def to_s
+    "%{F#{C.color}}%{R} #{C.icon} %{R}%{F-} #{percentage.to_i}%"
   end
 
-  def self.used
-    readmemory.inject(:-)
+  def free
+    @free + @buffers + @cached
   end
 
-  def self.percentage
-    tmp = readmemory
-    (tmp.inject(:-) / tmp.first) * 100
+  def used
+    @total - free
   end
 
-  def self.monitor(rate = 0.5)
-    Thread.new do
+  def percentage
+    (used / @total) * 100
+  end
+
+  def monitor
       loop do
-        yield self
-        sleep rate
+        initialize
+        yield
+        sleep C.rate
       end
-    end
   end
 end
