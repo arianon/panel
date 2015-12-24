d7f9b4b98e5de4aa1d3b06ed2e534efd117fc0de
arianon <arianon@openmailbox.org>
2015-12-23 19:42:16 -0430

    pulseaudio.rb: Rewritten to obey config.yml

--- lib/widgets/pulseaudio.rb
+++ lib/widgets/pulseaudio.rb
@@ -1,35 +1,41 @@
-#!/usr/bin/ruby -wU
+require_relative '../config'
 
 class PulseAudio
-  attr_reader :volume, :muted
-  alias_method :muted?, :muted
+  C = CONFIG.volume
 
   def initialize
     pactl = `pactl list sinks`
-
-    @volume = pactl[%r{^\s*Volume:.*/\s+(\d+)%\s+/}, 1].to_i
+    @volume = pactl[/[0-9]+%/].to_i
     @muted = pactl[/Mute: (yes|no)/, 1] == 'yes'
+    system "pactl set-sink-volume 0 100%" if @volume > 100
   end
 
-  def monitor
-    Thread.new do
-      yield
-      open('| pactl subscribe change').each_line do |line|
-        # Respond only to sink changes
-        if line.include? 'sink'
-          update!
-          yield
-        end
-      end
-    end
+  def icon
+    n = @muted ? 1 : 0
+    "%{F#{C.colors[n]}}%{R} #{C.icons[n]} %{R}%{F-} "
   end
 
-  def update!
-    initialize
-    system 'pactl set-sink-volume 0 100%' if @volume > 100
+  def volume
+      "#{@volume}%"
   end
 
   def to_s
-    "#{@volume}:#{@muted ? 'on' : 'off'}"
+    widget = ''
+    buttons = C.buttons.to_h
+
+    widget << buttons.map { |btn, cmd| "%{A#{btn}:#{cmd}:}" }.join
+    widget << icon << volume
+    widget << '%{A}' * buttons.size
+  end
+
+  def monitor
+    yield
+    open('| pactl subscribe change').each_line do |line|
+      # Respond only to sink changes
+      if line.include? 'sink'
+        initialize
+        yield
+      end
+    end
   end
 end
